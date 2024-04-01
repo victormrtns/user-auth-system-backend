42b1571eb6a52b8095ffdf3fe19a5856c7cac58e
Victor <88071929+victormrtns@users.noreply.github.com>
2024-04-01 20:29:23 -0300

    [perf] - Putting async and await func inside controller and service

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Get,Post } from "@nestjs/common";
 import { CreateUserDTO } from "src/dto/create-user-dto";
+import { User } from "src/entity/user.entity";
 import { UserService } from "src/service/user.service";
 @Controller('/user')
 export class UserController{
   constructor(readonly userService:UserService){}
   @Post('post')
-  async postUser(@Body() request_user:CreateUserDTO): Promise<string> {
+  async postUser(@Body() request_user:CreateUserDTO){
     const user = await this.userService.create(request_user)
     return user;
   }
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { CreateUserDTO } from "src/dto/create-user-dto";
+import { User } from "src/entity/user.entity";
 
 @Injectable()
 export class UserService{
-  async create(createUserDTO:CreateUserDTO){
-    return await "hello";
+  async create(createUserDTO:CreateUserDTO):Promise<User>{
+    const user = new User();
+    user.username = "aaaaaaaaaaaaa"
+    user.name = "victorrr"
+    user.last_name = "aaaaaa"
+    user.email = "vmhugo00@gmail.com"
+    return user;
   }
 }
